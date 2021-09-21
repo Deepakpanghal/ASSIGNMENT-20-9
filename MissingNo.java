f7af3f0aece393bf3bd54654eca6d5439b704d84
Deepakpanghal <91010930+Deepakpanghal@users.noreply.github.com>
2021-09-21 18:51:05 +0530

    Add files via upload

---
+++ MissingNo.java
@@ -0,0 +1,19 @@
+package com.geekster.assignment;
+
+public class MissingNo {
+
+	public static void main(String args[]) {
+		// TODO Auto-generated method stub
+		
+		int arr[] = {1,2,3,4,5,6,8,9,10,11,12,13,14,15};
+		int sum = 0;
+		int n = 15;
+		int sumofnnumbers = (n*(n+1))/2;
+		for(int i = 0;i <arr.length;i++) {
+			sum += arr[i];
+		}
+		System.out.println("Missing Number : "+(sumofnnumbers-sum));
+
+	}
+
+}
